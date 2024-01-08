d84b1280451a29e546511ab59af854d79299c0d3
rushil2501 <shah.9@iitj.ac.in>
2024-01-09 00:45:15 +0530

    vercel

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export default async function connect(){
-    await mongoose.connect('mongodb+srv://admin:admin123@quiz.2nbaf6w.mongodb.net/')
+    await mongoose.connect(process.env.ATLAS_URI)
     console.log("Database Connected")
 }
