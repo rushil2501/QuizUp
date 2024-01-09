2d076252d31f8c9a684424af2abdd7c5a4f55299
rushil2501 <shah.9@iitj.ac.in>
2024-01-09 23:26:17 +0530

    Update Questions.js

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -28,29 +28,29 @@ export default function Questions({onChecked}) {
     }
     if(isLoading)return <h3 className='text-light'>isLoading</h3>
     if(serverError)return <h3 className='text-light'>{serverError || "Unknown Error"}</h3>
-    console.log("cpt3")
+    console.log("cpt3",questions?.question)
     return (
     
     <div className='questions'>
         <h2 className='text-light'> {questions?.question}</h2> 
-        // <ul key={questions?.id}>
-        //     {
-        //     questions?.options.map((q,i) => (
-        //         <li key ={i}>
-        //                 <input 
-        //                     type="radio"
-        //                     value={false}
-        //                     name="options"
-        //                     id={`q${i}-option`}
-        //                     onChange={() => onSelect(i)}
-        //                 />
+        <ul key={questions?.id}>
+            {
+            questions?.options.map((q,i) => (
+                <li key ={i}>
+                        <input 
+                            type="radio"
+                            value={false}
+                            name="options"
+                            id={`q${i}-option`}
+                            onChange={() => onSelect(i)}
+                        />
 
-        //                 <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
-        //                 <div className= {`check ${result[trace]===i?'checked':''}`}></div>
-        //             </li>
-        //     ) )
-        //     }
-        // </ul>
+                        <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+                        <div className= {`check ${result[trace]===i?'checked':''}`}></div>
+                    </li>
+            ) )
+            }
+        </ul>
       
     </div>
   )
