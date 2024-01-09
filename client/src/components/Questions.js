ce23f3932c69c0cb35e44da95a115ba09306e692
rushil2501 <shah.9@iitj.ac.in>
2024-01-09 23:24:22 +0530

    Update Questions.js

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -33,24 +33,24 @@ export default function Questions({onChecked}) {
     
     <div className='questions'>
         <h2 className='text-light'> {questions?.question}</h2> 
-        <ul key={questions?.id}>
-            {
-            questions?.options.map((q,i) => (
-                <li key ={i}>
-                        <input 
-                            type="radio"
-                            value={false}
-                            name="options"
-                            id={`q${i}-option`}
-                            onChange={() => onSelect(i)}
-                        />
+        // <ul key={questions?.id}>
+        //     {
+        //     questions?.options.map((q,i) => (
+        //         <li key ={i}>
+        //                 <input 
+        //                     type="radio"
+        //                     value={false}
+        //                     name="options"
+        //                     id={`q${i}-option`}
+        //                     onChange={() => onSelect(i)}
+        //                 />
 
-                        <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
-                        <div className= {`check ${result[trace]===i?'checked':''}`}></div>
-                    </li>
-            ) )
-            }
-        </ul>
+        //                 <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+        //                 <div className= {`check ${result[trace]===i?'checked':''}`}></div>
+        //             </li>
+        //     ) )
+        //     }
+        // </ul>
       
     </div>
   )
