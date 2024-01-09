e4d4393f5331664eef2a39791e2f542fd8a2ba32
rushil2501 <shah.9@iitj.ac.in>
2024-01-09 23:14:47 +0530

    Update Quiz.js

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -51,7 +51,16 @@ export default function Quiz() {
     <div className='container'>
       <h1 className='title text-light'>Quiz Application</h1>
       {/* displaying questions */}
-      
+      <Questions onChecked={onChecked} />
+
+
+
+      <div className='grid'>
+        {trace> 0 ?         <button className='btn prev' onClick={onPrev}>Prev</button>
+ :<div></div>}
+        <button className='btn next' onClick={onNext}>Next</button>
+ 
+      </div>
     </div>
   )
 }
